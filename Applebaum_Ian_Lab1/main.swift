434a1760e0fdb4f5c370d40b738c7abe87249c04
Ian Applebaum <Ian.tyler@electricbluefish.com>
2020-02-13 11:31:53 -0500

    Initial Commit

---
+++ Applebaum_Ian_Lab1/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  Applebaum_Ian_Lab1
+//
+//  Created by Ian Applebaum on 2/13/20.
+//  Copyright © 2020 Ian Applebaum. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
